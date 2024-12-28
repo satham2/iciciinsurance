004527d847e48a646693adf3987628b7e1a639ce
satham2 <sathamsmart2@gmail.com>
2024-12-28 16:00:25 +0530

    new code

---
+++ src/org/test/Employee.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		
+		while (a>5) {
+			a++;
+			System.out.println("yes");
+		}
+	System.out.println("No");
+	}
+
+}
